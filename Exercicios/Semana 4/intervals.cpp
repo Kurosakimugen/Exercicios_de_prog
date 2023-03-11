ed55978995c04f08abbb1e847d2d377b13feb952
Luis <firefallinfinity@gmail.com>
2023-03-11 20:01:43 +0000

    Resoluçao do 5

--- Exercicios/Semana 4/intervals.cpp
+++ Exercicios/Semana 4/intervals.cpp
@@ -1,48 +1,40 @@
 #include "interval.h"
 
-bool Maior_que(time_of_day a,time_of_day b) {
-    bool resultado;
-    if (a.h > b.h) {
-        resultado =  true;
-        return resultado;
-    }
-    if (a.h < b.h) {
-        resultado = false;
-        return resultado;
-    }
-    if (a.h == b.h) {
-        if (a.m > b.m) {
-            resultado =  true;
-            return resultado;
-        }
-        resultado = false;
-    }
-    return resultado;
+bool operator<(time_of_day a, time_of_day b){ //funçao que permite utilizar apenas o operador < para comparar
+  return a.h < b.h || (a.h==b.h && a.m<b.m);
+}
+bool operator==(time_of_day a, time_of_day b){ //funçao que permite utilizar apenas o operador == para comparar
+  return a.h == b.h && a.m == b.m;
+}
+bool operator>(time_of_day a, time_of_day b){  //funçao que permite utilizar apenas o operador  > para comparar
+  return b < a;
+}
+bool operator>=(time_of_day a, time_of_day b){ //funçao que permite utilizar apenas o operador >= para comparar
+  return a > b || a == b;
+}
+bool operator<=(time_of_day a, time_of_day b){ //funçao que permite utilizar apenas o operador <= para comparar
+  return a < b || a == b;
 }
 
 int search_intervals(time_of_day t, const interval a[], int n, interval& u) {
     int resultado = 0;
     int temp = 0;
+    u.start = t;
+    u.end = t;
     for (int i = 0; i < n; i++) {
-        if (Maior_que(t,a[i].start)) { //verificar que o tempo pertence ao intervalo
-            if (Maior_que(a[i].end,t) == false) { //verificar que o tempo esta abaixo do limite
-                temp = a[i].end.h - a[i].start.h; //soma do tempo do intervalo
-                temp *= 60;
-                resultado += temp;
-                resultado += (a[i].end.m - a[i].start.m);
-                if (i == 0) {
-                    u.start = a[i].start;
-                    u.end = a[i].end;
-                }
-                if (Maior_que(u.start,a[i].start)) {//verificar que aba de baixo é mais pequena das interseções
-                    u.start = a[i].start;
-                }
-                if (Maior_que(a[i].end, u.end)) {
-                    u.end = a[i].end;
-                }
+        if (t >= a[i].start && t < a[i].end) {
+            if (u.start >= a[i].start) { //verificar que aba de baixo é mais pequena das interseções
+                u.start = a[i].start;
+            }
+            if (a[i].end >= u.end) {
+                u.end = a[i].end;
             }
         }
     }
+    temp = u.end.h - u.start.h; //soma do tempo do intervalo
+    temp *= 60;
+    resultado += temp;
+    resultado += (u.end.m - u.start.m);
     return resultado;
 }
 
