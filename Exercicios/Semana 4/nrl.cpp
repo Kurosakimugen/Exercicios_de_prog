670972a28d68c031db01791187dd44f07e8e09d1
Luis <firefallinfinity@gmail.com>
2023-03-11 20:33:52 +0000

    Finalizção do exercicio 4

--- Exercicios/Semana 4/nrl.cpp
+++ Exercicios/Semana 4/nrl.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 int nrl(const char s[], char low[]) {
-    char teste[27] =  {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    char alfabeto[27] = {"abcdefghijklmnopqrstuvwxyz"};
+    int alfabeto_unico[26] = {0};
     int resultado = 0;
     int i = 0;
     while (s[i] != '\0') {
@@ -13,53 +12,46 @@ int nrl(const char s[], char low[]) {
             continue;
         }
         if (s[i] >= 'a' && s[i] <= 'z') {
-            int letra = int(s[i]) - int('a');
-            if (teste[letra] == char(letra + 65)) {
-                teste[letra] = alfabeto[letra];
+            int letra = s[i] - 'a';
+            if (alfabeto_unico[letra] == 0) {
+                alfabeto_unico[letra]++; //contabiliza a primeira ocorrencia
+                i++;
+                continue;
             }
-            else {
-                teste[letra] = '-';
+            if (alfabeto_unico[letra] == 1) {
+                alfabeto_unico[letra] = 2; //já não é contabilizado porque tem de ser unico
             }
             i++;
             continue;
         }
         if (s[i] >= 'A' && s[i] <= 'Z') {
-            int letra = int(s[i]) - int('A');
-            if (teste[letra] == char(letra)) {
-                teste[letra] = alfabeto[letra];
+            int letra = s[i] - 'A';
+            if (alfabeto_unico[letra] == 0) {
+                alfabeto_unico[letra]++; //contabiliza a primeira ocorrencia
+                i++;
+                continue;
             }
-            else {
-                teste[letra] = '-';
+            if (alfabeto_unico[letra] == 1) {
+                alfabeto_unico[letra] = 2; //já não é contabilizado porque tem de ser unico
             }
             i++;
             continue;
         }
     }
     i = 0;
-    while (teste[i] != '\0') {
-        if (teste[i] == '-') {
-            i++;
-            continue;
+    int j = 0;
+    while(i != 26) {
+        if (alfabeto_unico[i] == 1) {
+            resultado++; //adicionar ao contador as varias letras que eram unicas
+            low[j] = 'a'+i; //adicionar à expressao de retorno a letra ocorrida
+            j++; 
         }
-        else {
-            low += alfabeto[i];
-            i++;
-            continue;
-        }
-    }
-    i = 0;
-    while(low[i] != '\0') {
-        resultado++;
         i++;
     }
+    low[j] = '\0'; //fechar a string
     return resultado;
 }
 
 int main() {
-    const char s[] = "  F C U P  F E U P  Porto  ";
-    char l[27] = { -1 };
-    int r = nrl(s, l);
-    cout << '\"' << s << "\" "
-       << r << " \"" << l << "\"\n";
     return 0;
 }
